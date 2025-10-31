c44c7af146b8c10afbb8a452416c34c2e87a1d57
Boshitha Gunarathna <IT21319792@my.sliit.lk>
2025-11-01 03:58:01 +0530

    Create Volunteers.tsx

---
+++ src/screens/Volunteers/Volunteers.tsx
@@ -0,0 +1,13 @@
+export const Volunteers = (): JSX.Element => {
+  return (
+    <div className="flex-1 p-6 overflow-y-auto">
+      <div className="flex items-center justify-between mb-6">
+        <h1 className="[font-family:'Inter',Helvetica] font-bold text-black text-[32px]">
+          Volunteers
+        </h1>
+      </div>
+
+      <p className="text-2xl text-gray-400 text-center mt-20">dummy page</p>
+    </div>
+  );
+};
