d1627d2eac5d574bceb8bf50b746dd0ef99b390e
Boshitha Gunarathna <IT21319792@my.sliit.lk>
2025-11-01 03:57:57 +0530

    Create SocialMedia.tsx

---
+++ src/screens/SocialMedia/SocialMedia.tsx
@@ -0,0 +1,13 @@
+export const SocialMedia = (): JSX.Element => {
+  return (
+    <div className="flex-1 p-6 overflow-y-auto">
+      <div className="flex items-center justify-between mb-6">
+        <h1 className="[font-family:'Inter',Helvetica] font-bold text-black text-[32px]">
+          Social Media
+        </h1>
+      </div>
+
+      <p className="text-2xl text-gray-400 text-center mt-20">dummy page</p>
+    </div>
+  );
+};
